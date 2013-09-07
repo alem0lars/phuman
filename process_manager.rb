d9967b45af94b62d8324afc17743f95cc58ecae4
Luca Molari <molari.luca@gmail.com>
2013-09-07 14:15:44 +0200

    Shebang

--- process_manager.rb
+++ process_manager.rb
@@ -1,3 +1,6 @@
+#/usr/bin/env ruby
+
+
 # { Configuration
 
 $config = {
@@ -51,6 +54,7 @@ def main
   end while(pman.get_affinity == $config[:desired_affinity]) &&
            ($config[:max_retries] == nil || retries_idx < $config[:max_retries])
 end
+
 
 # Start the program
 main
